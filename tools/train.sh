c456c695acd9f45887ecd2578927c44f3669798b
Shilong Zhang <61961338+jshilong@users.noreply.github.com>
2023-03-21 08:59:48 +0800

    fix train.sh

--- tools/train.sh
+++ tools/train.sh
@@ -7,4 +7,4 @@ PORT=${PORT:-29500}
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH
 echo $PYTHONPATH
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train.py   --seed 0 $CONFIG --launcher pytorch ${@:3} --work-dir $WORKDIR
+    $(dirname "$0")/train.py   --seed 0 $CONFIG --launcher pytorch ${@:4} --work-dir $WORKDIR
